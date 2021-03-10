fafd40fcfde2c74897e0fb2dd2e6aaa16437eea1
Sterling <sterlingcduke@gmail.com>
2021-03-10 00:01:45 -0600

    file setup jumbotron component

---
+++ src/components/Jumbotron.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Jumbotron() {
+  return (
+    <div className="jumbotron jumbotron-fluid">
+      <div className="container">
+        <h1 className="display-4">Employee Directory</h1>
+        <p className="lead">This is a Employee Directory</p>
+      </div>
+    </div>
+  );
+}
+
+export default Jumbotron;
+
+
+
+
